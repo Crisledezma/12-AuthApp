0e69552ff9a5b0f2cca99d3ca557207ad6927264
Cristian Ledezma <crisledezma@gmail.com>
2023-02-11 20:51:20 -0600

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'localhost:4000/api/auth/'
+  baseUrl: 'https://12-auth-server-production-2f2e.up.railway.app/api/auth/'
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:4000/api/auth/'
+  baseUrl: 'https://12-auth-server-production-2f2e.up.railway.app/api/auth/'
 }
